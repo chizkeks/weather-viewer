56c8ade01fd989bcd3dae9c97375974a5d65ed10
Ilya <pavlov.ilya.y@gmail.com>
2024-11-29 00:49:35 +0300

    Initial commit

---
+++ src/test/java/ru/petprojects/chizkeks/weather_viewer/WeatherViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.petprojects.chizkeks.weather_viewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
